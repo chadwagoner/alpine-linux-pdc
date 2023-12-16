1d65696b853733c59fea2c642aae4b280e08c10c
Chad <chawagon03@gmail.com>
2023-12-16 14:31:31 -0500

    fix(init): made commands less noisy

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ HEADER_NO_CACHE="Cache-Control: no-cache, no-store"
 
 ### INSTALL REQUIRED BASE PACKAGES
 echo -e "INSTALLING BASE PACKAGES"
-apk add -U curl jq lm-sensors ncurses pciutils yq
+apk add -U curl jq lm-sensors ncurses pciutils yq --quiet
 echo -e ""
 
 ### CREATE CONFIG DIRECTORY (IF DOESN'T EXIST)
@@ -52,10 +52,10 @@ if [[ "$(stat -c '%a' /root/.config/alpine-linux-pdc/config.yaml)" != 600 ]]; th
 fi
 
 ### ADD OS-UPDATER TO CRON (/etc/periodic/monthly)
-curl -L -o /etc/periodic/monthly/os-updater -H '$HEADER_NO_CACHE' $REPOSITORY_URL/os-updater
+curl -L -o /etc/periodic/monthly/os-updater -H '$HEADER_NO_CACHE' $REPOSITORY_URL/os-updater --silent
 
 ### ADD PDC TO CRON (/etc/periodic/hourly)
-curl -L -o /etc/periodic/hourly/pdc -H '$HEADER_NO_CACHE' $REPOSITORY_URL/pdc
+curl -L -o /etc/periodic/hourly/pdc -H '$HEADER_NO_CACHE' $REPOSITORY_URL/pdc --silent
 
 ### ADD PDC-UPDATER TO CRON (15min)
-curl -L -o /etc/periodic/15min/pdc-updater -H '$HEADER_NO_CACHE' $REPOSITORY_URL/pdc-updater
+curl -L -o /etc/periodic/15min/pdc-updater -H '$HEADER_NO_CACHE' $REPOSITORY_URL/pdc-updater --silent
